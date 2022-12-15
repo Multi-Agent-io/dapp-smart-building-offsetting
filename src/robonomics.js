98392bf66edc0ad2b7905f7d334a7e51d9a48820
vol4tim <vol4tim@gmail.com>
2022-12-15 15:03:11 +0400

    fix endpoint

--- src/robonomics.js
+++ src/robonomics.js
@@ -3,11 +3,10 @@ import {
   AccountManagerUi as AccountManager,
   Robonomics
 } from "robonomics-interface";
-// import AccountManager from "./utils/accountManagerDapp";
 
 const config = {
   endpoint:
-    process.env.NODE_ENV !== "production"
+    process.env.NODE_ENV === "production"
       ? "wss://kusama.rpc.robonomics.network/"
       : "ws://127.0.0.1:9944/",
 
